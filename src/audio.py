7d32deeffbfe0519d33cd077769283720dff4d4b
dawetmaster <andika.naufal48@gmail.com>
2023-12-22 09:54:18 +0700

    [CHORE] penanganan request

--- src/audio.py
+++ src/audio.py
@@ -2,13 +2,47 @@ import librosa
 import numpy as np
 from scipy.signal import medfilt
 
-def median_filter(signal, kernel_size=3):
-    return medfilt(signal, kernel_size)
+def verify_audio_header(content_type):
+    if content_type is not None:
+        return "audio/" in content_type or "multipart/form-data" in content_type
+    else:
+        return False
 
-# Fungsi untuk mengurangi noise dengan spektral subtraksi
-def spectral_subtraction(signal, noise, alpha=1):
-    signal_energy = np.sum(np.abs(signal)**2)
-    noise_energy = np.sum(np.abs(noise)**2)
-    reduction_factor = np.maximum(1.0 - alpha * (noise_energy / signal_energy), 0)
-    denoised_signal = signal * reduction_factor
-    return denoised_signal
+# Fungsi untuk ekstraksi fitur MFCC
+def extract_mfcc(audio, sr, n_mfcc=13):
+    mfccs = librosa.feature.mfcc(y=audio, sr=sr, n_mfcc=n_mfcc)
+    return mfccs
+
+# Fungsi untuk normalisasi fitur-fitur MFCC
+def normalize_mfcc(mfcc_features):
+    mean = np.mean(mfcc_features, axis=1, keepdims=True)
+    std = np.std(mfcc_features, axis=1, keepdims=True)
+    normalized_mfcc = (mfcc_features - mean) / (std + 1e-8)
+    return normalized_mfcc
+
+def process_audio(audio_req):
+    audio, sr = librosa.load(audio_req, sr=None)
+    audio = np.asarray(audio)
+    mfcc = extract_mfcc(audio, sr=sr)
+    normalized_mfcc = normalize_mfcc(mfcc)
+    return normalized_mfcc
+
+# # Membaca file audio
+# new_audio, new_sr = librosa.load(new_audio_file, sr=None)
+
+# # Memastikan bahwa audio dalam bentuk numpy.ndarray
+# new_audio = np.asarray(new_audio)
+
+# # Ekstraksi fitur MFCC
+# new_mfcc = extract_mfcc(new_audio, sr=new_sr)
+
+# # Normalisasi fitur-fitur MFCC
+# normalized_new_mfcc = normalize_mfcc(new_mfcc)
+
+# # Contoh: Membuat prediksi pada satu contoh audio baru
+# new_audio_file = '/content/sample_data/audio/LJ001-0002.wav'  # Ganti dengan path file audio baru Anda
+# new_mfcc = extract_mfcc(new_audio_file, sr=22050)  # Ganti dengan nilai sampling_rate yang sesuai
+# normalized_new_mfcc = normalize_mfcc(new_mfcc)
+
+# # Reshape data sesuai dengan bentuk input model
+# input_data = normalized_new_mfcc.reshape((1, n_mfcc, target_length))
--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 import os
 import librosa
 import numpy as np
+from pydantic import BaseModel
 from dotenv import load_dotenv
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile, Request, Response, status
 from tensorflow.keras.models import load_model
+from typing import Annotated
+from src.audio import process_audio, verify_audio_header
 
 # Initialization
 load_dotenv()
@@ -21,12 +24,23 @@ async def greet():
     }
     return results
 
-@app.post('/predict')
-async def predict(request):
-    results = {
-        'status': 200,
-        'data': {
-            'message': 'Hello, Optimiz!'
+@app.post('/predict-dummy', status_code=201)
+async def predict_dummy():
+    return {'suara': 'lancar'}
+
+@app.post('/predict', status_code=201)
+async def predict(
+    req: Request, 
+    res: Response,
+    file: Annotated[UploadFile, File(description="A file read as UploadFile")]
+    ):
+    if not verify_audio_header(req.headers.get('Content-Type')):
+        res.status_code = status.HTTP_400_BAD_REQUEST
+        return {
+            'message': 'Invalid file content type.',
+            'type': req.headers.get('Content-Type')
         }
+    return {
+        'headers': req.headers,
+        'file': file
     }
-    return results
